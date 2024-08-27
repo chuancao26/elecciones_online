d1c55b6017cd90f5df99309dd28d27027513fff9
chuancao26 <chuancao@unsa.edu.pe>
2024-08-27 09:30:09 -0500

    adding database tables and modifying user and password

--- app/models.py
+++ app/models.py
@@ -8,22 +8,27 @@ class Persona(Base):
     nombres = Column(String, nullable = False)
     apellido_paterno = Column(String, nullable = False)
     apellido_materno = Column(String, nullable = False)
-    usuario = Column(String, unique = True, nullable = True)
-    password = Column(String, nullable=False)
+
 class Elector(Base):
     __tablename__ = "elector"
     id = Column(Integer, primary_key=True, autoincrement=True)
     id_persona = Column(Integer, ForeignKey("persona.id", ondelete="CASCADE"), nullable=True)
+    usuario = Column(String, unique = True, nullable = True)
+    password = Column(String, nullable=False)
+
 class Administrador(Base):
     __tablename__ = "administrador"
     id = Column(Integer, primary_key=True, autoincrement=True)
     id_administrador= Column(Integer, ForeignKey("persona.id", ondelete="CASCADE"), nullable=True)
+    usuario = Column(String, unique = True, nullable = True)
+    password = Column(String, nullable=False)
+
 class Eleccion(Base):
     __tablename__ = "eleccion"
     id = Column(Integer, primary_key=True, autoincrement=True)
     fecha = Column(TIMESTAMP(timezone=True), nullable=False)
-    hora_inicio = Column(Time, nulleable=False)
-    hora_fin = Column(Time, nulleable=False)
+    hora_inicio = Column(Time, nullable=False)
+    hora_fin = Column(Time, nullable=False)
     descripcion = Column(String, nullable=False, server_default="")
 
 class Lista(Base):
@@ -32,9 +37,9 @@ class Lista(Base):
     nombre = Column(String, nullable=False)
     id_eleccion = Column(Integer, ForeignKey("eleccion.id", ondelete="CASCADE"), nullable=False)
     propuesta = Column(String, nullable=False, server_default="")
+
 class Candidato(Base):
     __tablename__ = "candidato"
     id = Column(Integer, primary_key=True, autoincrement=True)
     id_lista = Column(ForeignKey("lista.id", ondelete="CASCADE"), nullable=False)
-    id_persona
-
+    id_persona = Column(ForeignKey("persona.id", ondelete="CASCADE"), nullable=False)

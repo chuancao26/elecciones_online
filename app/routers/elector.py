6116adb97c291f23f0c798913b12576c9f3e00c8
chuancao26 <chuancao@unsa.edu.pe>
2024-08-27 23:07:13 -0500

    adding utils tool

--- app/routers/elector.py
+++ app/routers/elector.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from app.database import get_db
+from app.utils import hash_password, verify_password
 from typing import List
 
 from app import schemas, models
@@ -16,15 +17,16 @@ def create_persona(new_persona: models.Persona, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_persona)
 
-@router.get("/", response_model=List[schemas.PersonaCreate])
+@router.get("/", response_model=List[schemas.PersonaOut])
 def get_persona(db: Session = Depends(get_db)):
     personas = db.query(models.Persona).all()
     return personas
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PersonaCreate)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PersonaOut)
 def create_elector(elector: schemas.PersonaCreate, db: Session = Depends(get_db)):
     persona = db.query(models.Persona).filter(models.Persona.usuario == elector.usuario).first()
     if persona:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Elector ya existe")
+    elector.password = hash_password(elector.password)
     new_persona = models.Persona(**elector.model_dump())
     create_persona(new_persona, db)
     persona = db.query(models.Persona).filter(models.Persona.usuario == elector.usuario).first()

53761bdcb8a5f61dca2695e0c1f1d610c49f8e25
chuancao26 <109006392+chuancao26@users.noreply.github.com>
2024-08-29 23:55:50 -0500

    running on windows

--- app/routers/eleccion.py
+++ app/routers/eleccion.py
@@ -5,8 +5,7 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from typing import List
 
-from app import schemas
-from app import models
+from app import schemas, models, oauth2
 
 router = APIRouter(prefix="/eleccion",
                    tags=["eleccion"])
@@ -16,20 +15,23 @@ def get_elections(db: Session = Depends(get_db)):
     return elections
 
 @router.post("/", response_model=schemas.EleccionOut)
-def create_election(election: schemas.EleccionCreate, db: Session = Depends(get_db)):
+def create_election(election: schemas.EleccionCreate, db: Session = Depends(get_db), 
+                    current_user: schemas.TokenData = Depends(oauth2.get_current_admin)):
     new_election = models.Eleccion(**election.model_dump())
     db.add(new_election)
     db.commit()
     db.refresh(new_election)
     return new_election
 @router.get("/{id}", response_model=schemas.EleccionOut, status_code=status.HTTP_201_CREATED)
-def get_election(id: int, db: Session = Depends(get_db)):
+def get_election(id: int, db: Session = Depends(get_db),
+                 current_user: schemas.TokenData = Depends(oauth2.get_current_admin)):
     election = db.query(models.Eleccion).filter(models.Eleccion.id == id)
     if election.first() is None:
         raise HTTPException(status_code=404, detail=f"id: {id} not found")
     return election.first()
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_election(id: int, db: Session = Depends(get_db)):
+def delete_election(id: int, db: Session = Depends(get_db), 
+                    current_user: schemas.TokenData = Depends(oauth2.get_current_admin)):
     election = db.query(models.Eleccion).filter(models.Eleccion.id == id)
     if election.first() is None:
         raise HTTPException(status_code=404, detail=f"id: {id} not found")
@@ -37,7 +39,8 @@ def delete_election(id: int, db: Session = Depends(get_db)):
     db.commit()
     return HTTPException(status_code=status.HTTP_204_NO_CONTENT)
 @router.put("/{id}", response_model=schemas.EleccionOut, status_code=status.HTTP_200_OK)
-def update_election(election: schemas.EleccionCreate, id: int, db: Session = Depends(get_db)):
+def update_election(election: schemas.EleccionCreate, id: int, db: Session = Depends(get_db),
+                    current_user: schemas.TokenData = Depends(oauth2.get_current_admin)):
     updated_election = db.query(models.Eleccion).filter(models.Eleccion.id == id)
     if updated_election.first() is None:
         raise HTTPException(status_code=404, detail=f"id: {id} not found")

6605ecbb6ebc2e9839b4f09fcedc0bff157c7067
chuancao26 <chuancao@unsa.edu.pe>
2024-11-09 14:34:32 -0500

    adding selenium and other libraries

--- tests/test_listas.py
+++ tests/test_listas.py
@@ -1,4 +1,3 @@
 def test_get_all_listas(create_elections, create_listas, authorized_admin_client):
     listas = authorized_admin_client.get('/lista/')
-    print(listas.json())
     assert listas.status_code == 200
